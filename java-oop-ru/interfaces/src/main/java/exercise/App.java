17f94952977f89fd0b6f914482f6be57aeb0a183
@hexlet/cli <support@hexlet.io>
2024-08-25 16:46:36 +0500

    submit java-oop-ru/interfaces

--- java-oop-ru/interfaces/src/main/java/exercise/App.java
+++ java-oop-ru/interfaces/src/main/java/exercise/App.java
@@ -6,28 +6,25 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 // BEGIN
-public class App{
-    public static List<String> buildApartmentsList(List<Home>list, int n) {
-        boolean a = list.isEmpty();
-        if((n>0&n<list.size())&a==false){
-List<String>res = new ArrayList<>();
-Comparator<Home>compareByArea = Comparator.comparing(Home::getArea);
-ArrayList<Home> sortedHome = list.stream()
-        .sorted(compareByArea)
-        .collect(Collectors.toCollection(ArrayList::new));
-for (Home list1 :sortedHome){
-    res.add(list1.toString());
-}
-List<String> res2 = new ArrayList<>();
-
-        for (int i = 0; i < n; i++) {
-            res2.add(res.get(i));
+public class App {
+    public static List<String> buildApartmentsList(List<Home> list, int n) {
+        List<String> result = new ArrayList<>();
+        if (n > 0 & n <= list.size()) {
+            List<String> res = new ArrayList<>();
+            Comparator<Home> compareByArea = Comparator.comparing(Home::getArea);
+            ArrayList<Home> sortedHome = list.stream()
+                    .sorted(compareByArea)
+                    .collect(Collectors.toCollection(ArrayList::new));
+            for (Home list1 : sortedHome) {
+                res.add(list1.toString());
+            }
+            List<String> res2 = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                res2.add(res.get(i));
+            }
+            result = res2;
         }
-        return res2;
-    } else {
-        ArrayList<String> now = new ArrayList<>();
-        return now;
-        }
+        return result;
     }
 }
 // END
--- java-oop-ru/interfaces/src/main/java/exercise/Main.java
+++ java-oop-ru/interfaces/src/main/java/exercise/Main.java
@@ -7,21 +7,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Online IDE!! Happy Coding :)");
 
-        Home flat = new Flat(54.5, 4, 3);
-        double area = flat.getArea(); // 58.5
-        System.out.println(flat.toString());
-
-        Home cottage = new Cottage(135, 2);
-        double area1 = cottage.getArea(); // 135
-        System.out.println(cottage.toString());
-
         List<Home> apartments = new ArrayList<>(List.of(
                 new Flat(41, 3, 10),
                 new Cottage(125.5, 2),
                 new Flat(80, 10, 2),
                 new Cottage(150, 3)
         ));
-        List<String> result = App.buildApartmentsList(apartments, 545);
+        List<String> result = App.buildApartmentsList(apartments, 0);
         System.out.println(result);
     }
 
