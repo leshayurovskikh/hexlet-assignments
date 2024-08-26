577894ca023dd523e5e3405017535ed4b35f1bed
@hexlet/cli <support@hexlet.io>
2024-08-26 22:32:20 +0500

    submit java-oop-ru/subtyping

--- java-oop-ru/subtyping/src/main/java/exercise/App.java
+++ java-oop-ru/subtyping/src/main/java/exercise/App.java
@@ -5,5 +5,13 @@ import java.util.Map.Entry;
 import java.util.Set;
 
 // BEGIN
+public class App {
 
+    public static void swapKeyValue (KeyValueStorage keyValueStorage) {
+        for (var entryStorage : keyValueStorage.toMap().entrySet()) {
+            keyValueStorage.unset(entryStorage.getKey());
+            keyValueStorage.set(entryStorage.getValue(), entryStorage.getKey());
+        }
+    }
+}
 // END
--- java-oop-ru/subtyping/src/main/java/exercise/InMemoryKV.java
+++ java-oop-ru/subtyping/src/main/java/exercise/InMemoryKV.java
@@ -5,7 +5,37 @@ import java.util.HashMap;
 
 // BEGIN
 public class InMemoryKV implements KeyValueStorage{
+private Map<String,String> map;
 
+ public InMemoryKV(Map<String, String> map) {
+  this.map = new HashMap<>(map);
+ }
 
+ @Override
+ public void set(String key, String value) {
+  map.put(key, value);
+ }
+
+ @Override
+ public void unset(String key) {
+map.remove(key);
+ }
+
+ @Override
+ public String get(String key, String defaultValue) {
+  String result = "";
+  if (map.containsKey(key)){
+   result = map.get(key);
+  }else{
+   result = defaultValue;
+  }
+  return result;
+ }
+
+ @Override
+ public Map<String, String> toMap() {
+
+  return new HashMap<>(map);
+ }
 }
 // END
