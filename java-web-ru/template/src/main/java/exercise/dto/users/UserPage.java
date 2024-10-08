022c1cadef67408ce1eec3a854407814e0cb0ade
@hexlet/cli <support@hexlet.io>
2024-10-08 11:36:24 +0500

    submit java-web-ru/template

---
+++ java-web-ru/template/src/main/java/exercise/dto/users/UserPage.java
@@ -0,0 +1,13 @@
+package exercise.dto.users;
+
+import exercise.model.User;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+@AllArgsConstructor
+@Getter
+// BEGIN
+public class UserPage {
+    private User user;
+}
+// END
