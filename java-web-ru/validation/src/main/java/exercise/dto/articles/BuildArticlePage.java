544b5bbda682076951f3898817303482f5286249
@hexlet/cli <support@hexlet.io>
2024-10-18 20:31:42 +0500

    submit java-web-ru/validation

--- java-web-ru/validation/src/main/java/exercise/dto/articles/BuildArticlePage.java
+++ java-web-ru/validation/src/main/java/exercise/dto/articles/BuildArticlePage.java
@@ -1,9 +1,3 @@
-package exercise.dto.articles;
-
-import io.javalin.validation.ValidationError;
-import java.util.Map;
-import java.util.List;
-
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.Getter;
