5ad08df034c9f64e28806ef31111b205e9ac484b
@hexlet/cli <support@hexlet.io>
2024-10-19 19:44:15 +0500

    submit java-web-ru/session

---
+++ java-web-ru/session/src/main/java/exercise/controller/SessionsController.java
@@ -0,0 +1,53 @@
+package exercise.controller;
+
+import static io.javalin.rendering.template.TemplateUtil.model;
+import exercise.dto.MainPage;
+import exercise.dto.LoginPage;
+import exercise.repository.UsersRepository;
+import static exercise.util.Security.encrypt;
+
+import exercise.util.Security;
+import io.javalin.http.Context;
+import io.javalin.http.NotFoundResponse;
+
+public class SessionsController {
+
+    // BEGIN
+
+    public static void index(Context ctx) {
+        var page = new MainPage(ctx.sessionAttribute("currentUser"));
+        ctx.render("index.jte", model("page", page));
+    }
+
+    public static void build(Context ctx) {
+        var page = new LoginPage(null, null);
+        ctx.render("build.jte", model("page", page));
+    }
+
+    public static void create(Context ctx) {
+        var name = ctx.formParam("name");
+        var password = Security.encrypt(ctx.formParam("password"));
+
+        try {
+            var user = UsersRepository.findByName(name)
+                    .orElseThrow(() -> new NotFoundResponse("Wrong username or password"));
+
+            if (user.getPassword().equals(password)) {
+                ctx.sessionAttribute("currentUser", name);
+                ctx.redirect("/");
+            } else {
+                throw new NotFoundResponse("Wrong username or password");
+            }
+        } catch (NotFoundResponse e) {
+            var page = new LoginPage(name, e.getMessage());
+            ctx.render("build.jte", model("page", page));
+        }
+    }
+
+    public static void destroy(Context ctx) {
+        ctx.sessionAttribute("currentUser", null);
+        ctx.redirect("/");
+    }
+        }
+    // END
+}
